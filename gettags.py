a312455849fc299258f610a42bec90662e587954
TrumpOwen <okessuman@st.ug.edu.gh>
2020-07-09 05:18:51 -0400

    Modified search programs and added BOOKS for APIs

--- gettags.py
+++ gettags.py
@@ -3,18 +3,21 @@
 import re 
 from urllib.parse import urlparse, quote 
 
-pattern = "(\s|/)(.*web.*)(\s|.pdf)"
+s = input("Enter word to search ")
+pattern = '^.*(\\[|\\b|[\\W])%ss?(\\b|[\\W]|\\]).*$' % (s) 
 inFile = "allbooks.txt"
-outFile = "all_web_books.txt"
+outFile = "all_%s_books.txt" %(s) 
 
 file1 = open(inFile, "r") ;
 file2 = open(outFile, "w");
 
+count = 0 
+
 for line in file1:
 	if re.search(pattern, line, re.I ):
 		parts = line.rsplit("/", 1)
-		formats = "[%s](%s)\n" % (parts[-1].strip(), parts[0] + "/" + quote(parts[1].strip()))
+		formats = "* [%s](%s)\n" % (parts[-1].strip(), parts[0] + "/" + quote(parts[1].strip()))
 		file2.write(formats)
-					
+		count += 1					
 
-
+input("\n\n%d Entries Found : Press Enter To Exit ... " % (count) )
